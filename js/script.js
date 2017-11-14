0224ae9e31a648d08f99329ff2c27ae241434c24
MatteoPenta <matteopx@gmail.com>
2017-11-14 21:54:48 +0100

    Implemented Draw / Erase toggle functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
 /*
   TO-DOs
-  * Add Draw / Erase buttons
   * Add New button
   * Draw only when key pressed
 */
 
+var state = 'hovered';
+
+/* Behaves accordingly to the name of the button clicked
+*/
+function buttonHandler() {
+  var buttonClicked = $(this).attr('name');
+
+  switch (buttonClicked) {
+    case "drawEraseToggle":
+      /*
+      Retrieves the current text of the button.
+      Cases:
+        > 'Erase'-> change to 'Draw', sets state to 'white';
+        > 'Draw'->change to 'Erase', sets state to 'hovered'.
+      */
+      var drawEraseCurrentState = $(this).text();
+
+      if (drawEraseCurrentState == "Erase") {
+        state = 'white';
+        $(this).text('Draw');
+      } else {
+        state = 'hovered';
+        $(this).text('Erase');
+      }
+
+      break;
+    case "new":
+
+      break;
+    default:
+
+  }
+}
+
 function main() {
-  var container = $('.container');
+  var container = $('.grid-wrapper');
   var boxesPerLine = 16; // starting value for n of boxes per each line
 
   // gets the width of the container to set
@@ -16,7 +49,6 @@ function main() {
   // WARNING: parseInt needs control cases
   var boxDimension = parseInt(containerWidth) / boxesPerLine;
 
-  console.log(boxDimension);
 
   /* draws a single line with all the boxes
    they will be split into different lines thanks to the flexbox */
@@ -25,13 +57,23 @@ function main() {
     box.css('width', boxDimension);
     box.css('height', boxDimension);
 
-    $('.container').append(box);
+    $('.grid-wrapper').append(box);
   }
 
+  /* Sets the state of the hovered box accordingly to
+  its previous state */
   $('.box').hover(function() {
-    $(this).addClass('hovered');
+    if (state === 'hovered') {
+      $(this).addClass('hovered');
+    } else {
+      $(this).removeClass('hovered');
+    }
   });
 
+  /* Button Click handlers */
+  $('.button').on('click', buttonHandler);
+
 }
+
 
 $(document).ready(main());
